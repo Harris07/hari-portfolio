b85f822494ca29fdb4c42ebb5773460b7428610c
Harris07 <>
2026-07-16 20:39:49 +0530

    Pin photo top to 290px to align with yellow line start

--- src/components/ExperienceSection.tsx
+++ src/components/ExperienceSection.tsx
@@ -435,8 +435,7 @@ export default function ExperienceSection() {
         style={{
           position: 'absolute',
           right: 0,
-          top: 0,
-          bottom: 0,
+          top: 290,
           width: PHOTO_W,
           height: 850,
           borderRadius: '16px 0 0 16px',
